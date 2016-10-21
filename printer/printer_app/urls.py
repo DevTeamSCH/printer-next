8e6b3d89e57692a0b423a8848d72631fe013cb4f
Feri <ferencmarkizay@gmail.com>
2016-10-21 21:05:22 +0200

    Uj nyomtato form

--- printer/printer_app/urls.py
+++ printer/printer_app/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^$|index', views.IndexView.as_view(), name='index'),
+    url(r'newprinter', views.NewPrinterView.as_view(), name="newPrinter"),
     url(r'client', views.ClientView.as_view(), name='client'),
     url(r'FAQ', views.FAQView.as_view(), name='FAQ'),
     url(r'profile', views.ProfileView.as_view(), name='profile'),
--- printer/printer_app/views.py
+++ printer/printer_app/views.py
@@ -1,5 +1,6 @@
 # from django.contrib.auth.decorators import login_required
 from django.views import generic
+from django.views.generic.edit import CreateView
 
 from printer_app import models
 
@@ -24,3 +25,14 @@ class FAQView(generic.TemplateView):
 # @login_required
 class ProfileView(generic.TemplateView):
     template_name = "printer_app/profile.html"
+
+# @login_required
+class NewPrinterView(CreateView):
+    model = models.Printer
+    fields = ['name', 'type', 'comment']
+    template_name_suffix = '_create'
+    success_url = '/index'
+
+    def form_valid(self, form):
+        form.instance.owner = models.User.objects.get(id=1);
+        return super(NewPrinterView, self).form_valid(form);
