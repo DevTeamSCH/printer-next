57b47638f5c96c072bd8526f8d52fbd14b8b369d
Ferenc Márki-Zay <ferencmarkizay@gmail.com>
2017-06-02 12:20:51 +0200

    Bugfixek, a megjegyzes csak akkor latszodik, ha van

--- printer/printer/urls.py
+++ printer/printer/urls.py
@@ -3,12 +3,12 @@ from django.conf.urls.i18n import i18n_patterns
 from django.contrib import admin
 
 urlpatterns = [
-    url('', include('social_django.urls', namespace='social')),
     url(r'^admin/', admin.site.urls),
     url(r'^rosetta/', include('rosetta.urls')),
 ]
 
 urlpatterns += i18n_patterns(
+    url('', include('social_django.urls', namespace='social')),
     url(r'', include('printer_app.urls')),
     prefix_default_language=False
 )
